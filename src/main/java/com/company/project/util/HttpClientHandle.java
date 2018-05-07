c5e79caec6850b10afd455cfe18817d552d598a6
chengwengao <657325704@qq.com>
2018-05-07 14:16:17 +0800

    httpclient模拟GET、POST请求

---
+++ src/main/java/com/company/project/util/HttpClientHandle.java
@@ -0,0 +1,198 @@
+package com.company.project.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.company.project.core.Result;
+import com.company.project.core.ResultGenerator;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.*;
+
+/**模拟GET,POST请求
+ * GET请求请求头参数示例：
+ * get.addHeader("Accept", "text/html");
+ get.addHeader("Accept-Charset", "utf-8");
+ get.addHeader("Accept-Encoding", "gzip");
+ get.addHeader("Accept-Language", "en-US,en");
+ get.addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22");
+ * Created by chengwengao on 2018/4/22.
+ */
+public class HttpClientHandle {
+
+    private static Logger logger = Logger.getLogger(HttpClientHandle.class);
+    private static String requestUrl = "http://api.zj.cmcconenet.com/devices/";
+
+    public static void main(String[] args) {
+        //get请求
+        //请求头参数
+//        Map<String, String> headerMap = new HashMap<>();
+//        headerMap.put("api-key", "hPq0FOr9diFI8qgVMiLKAUdQxC8=");
+//        System.out.println(doGet(requestUrl + "52502620", headerMap));
+
+        //post请求
+        String url = "http://localhost:9000/user/list";
+        Map params = new HashMap();
+        params.put("page", 0);
+        params.put("size", 5);
+        String result = doPost(url, params);
+        System.out.println(result);
+
+    }
+
+    /**
+     * get请求
+     * @return
+     */
+    public static String doGet(String url, Map<String, String> headerMap) {
+        try {
+            HttpClient client = new DefaultHttpClient();
+            //发送get请求
+            HttpGet request = new HttpGet(url);
+            //封装请求头header
+            for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+                request.addHeader(entry.getKey(), entry.getValue());
+            }
+
+            HttpResponse response = client.execute(request);
+
+            /**请求发送成功，并得到响应**/
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                /**读取服务器返回过来的json字符串数据**/
+                String strResult = EntityUtils.toString(response.getEntity());
+
+                System.out.println(strResult);
+                return strResult;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * post请求(用于key-value格式的参数)
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String doPost(String url, Map params){
+
+        BufferedReader in = null;
+        try {
+            // 定义HttpClient
+            HttpClient client = new DefaultHttpClient();
+            // 实例化HTTP方法
+            HttpPost request = new HttpPost();
+            request.setURI(new URI(url));
+
+            //设置参数
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            for (Iterator iter = params.keySet().iterator(); iter.hasNext();) {
+                String name = (String) iter.next();
+                String value = String.valueOf(params.get(name));
+                nvps.add(new BasicNameValuePair(name, value));
+
+                //System.out.println(name +"-"+value);
+            }
+            request.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+
+            HttpResponse response = client.execute(request);
+            int code = response.getStatusLine().getStatusCode();
+            if(code == 200){    //请求成功
+                in = new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent(),"utf-8"));
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+                String NL = System.getProperty("line.separator");
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + NL);
+                }
+
+                in.close();
+
+                return sb.toString();
+            }
+            else{   //
+                System.out.println("状态码：" + code);
+                return null;
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    /**
+     * post请求（用于请求json格式的参数）
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String doPost(String url, String params) throws Exception {
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);// 创建httpPost
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-Type", "application/json");
+        String charSet = "UTF-8";
+        StringEntity entity = new StringEntity(params, charSet);
+        httpPost.setEntity(entity);
+        CloseableHttpResponse response = null;
+
+        try {
+
+            response = httpclient.execute(httpPost);
+            StatusLine status = response.getStatusLine();
+            int state = status.getStatusCode();
+            if (state == HttpStatus.SC_OK) {
+                HttpEntity responseEntity = response.getEntity();
+                String jsonString = EntityUtils.toString(responseEntity);
+                return jsonString;
+            }
+            else{
+                logger.error("请求返回:"+state+"("+url+")");
+            }
+        }
+        finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
